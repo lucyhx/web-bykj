a27afd833313848c6e98e625410a88baa8dcad14
lucyhx <lucyhx@users.noreply.github.com>
2017-04-24 17:30:32 +0800

    fix index js

--- js/index.js
+++ js/index.js
@@ -16,6 +16,36 @@ $(document).ready(function () {
       }
       html += '</div>'
       return html
+    },
+    breakpoints: {
+      // when window width is <= 480px
+      480: {
+        slidesPerView: 1,
+        paginationCustomRender: function (swiper, current, total) {
+          var html = '<div class="text-center tip">'
+          for (var i = 0; i < total; i++) {
+            html += '<img class="st-img" src="image/'
+              + ((i === (current - 1 ) % total)
+                ? 'bluetip.png' : 'blacktip.png') + '">'
+          }
+          html += '</div>'
+          return html
+        }
+      },
+      // when window width is <= 640px
+      640: {
+        slidesPerView: 2,
+        paginationCustomRender: function (swiper, current, total) {
+          var html = '<div class="text-center tip">'
+          for (var i = 0; i < total; i++) {
+            html += '<img class="st-img" src="image/'
+              + ((i === (current - 1 ) % total || i === (current ) % total)
+                ? 'bluetip.png' : 'blacktip.png') + '">'
+          }
+          html += '</div>'
+          return html
+        }
+      }
     }
   })
 })
