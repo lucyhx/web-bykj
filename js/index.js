46b49400d9bcebcb416ec99bd05ce8f60791897b
lucyhx <lucyhx@users.noreply.github.com>
2017-04-24 14:30:43 +0800

    fix index cp swiper

--- js/index.js
+++ js/index.js
@@ -4,15 +4,13 @@ $(document).ready(function () {
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
     slidesPerView: 3,
-    centeredSlides: true,
-    spaceBetween: 30,
     loop: true,
     paginationType: 'custom',
     paginationClickable: true,
     paginationCustomRender: function (swiper, current, total) {
       var html = '<div class="text-center tip">'
       for (var i = 0; i < total; i++) {
-        if (i === (current + 5) % total || i === (current + 6) % total || i === (current + 7) % total) {
+        if (i === (current-1 ) % total || i === (current ) % total || i === (current + 1) % total) {
           html += '<img class="st-img" src="image/bluetip.png">'
         } else {
           html += '<img class="st-img" src="image/blacktip.png">'
